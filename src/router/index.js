e0a2a0a45c7a534d8f349666ccdb05494f39e0d6
Linson <854700937@qq.com~>
2022-03-20 00:01:32 +0800

    优化项目结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import demo from '../components/demo.vue'
-import DiscoverMusic from '../Views/DiscoverMusic.vue'
-import RecommendedMusic from '../Views/RecommendedMusic.vue'
-import LatestMusic from '../Views/LatestMusic.vue'
-import LatestMV from '../Views/LatestMV.vue'
-import PlayMusicList from '../Views/PlayMusicList.vue'
-import PlayMusicMV from '../Views/PlayMusicMV.vue'
-import searchMusic from '../Views/searchMusic.vue'
+
 Vue.use(VueRouter)
 
 const routes = [{
     path: '/',
-    name: "home",
     component: ()=>import('../Views/DiscoverMusic.vue'),
 
   },
@@ -44,10 +36,10 @@ const routes = [{
     path: '/searchMusic',
     component: ()=>import('../Views/searchMusic.vue'),
   },
-  {
-    path: '/logon',
-    component: ()=>import('../components/demo.vue'),
-  }
+  // {
+  //   path: '/logon',
+  //   component: ()=>import('../components/demo.vue'),
+  // }
 ]
 const router = new VueRouter({
   routes
